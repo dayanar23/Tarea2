50fd919d428a6609a215e788e3aba7f73da104d4
dayanar23 <dayana2301@gmail.com>
2015-04-21 17:54:08 -0430

    commit

---
+++ Tarea2/Tarea2.py
@@ -0,0 +1,18 @@
+'''
+Created on 21/04/2015
+
+@author: dayana
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
